e48d5af2c981f8ad7686d6f5c762a4489ce2e5de
Lobsang Norbu <50455820+lobnor2@users.noreply.github.com>
2024-11-23 13:20:46 -0500

    add footer text and set it center

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex justify-center text-gray-500 h-10 items-center">
+      made by lobnor
+    </div>
+  );
 };
 
 export default Footer;
