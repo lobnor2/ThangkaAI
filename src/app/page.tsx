d1b409123a0bad3aabbef8868b3da68720b1c717
Lobsang Norbu <50455820+lobnor2@users.noreply.github.com>
2024-11-23 23:41:53 -0500

    add new animation for p

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,40 @@ import { motion } from "motion/react";
 
 export default function Home() {
   return (
-    <motion.div
-      className="flex flex-col justify-center items-center h-[calc(100vh-4rem-2.5rem)] w-full"
-      initial={{
-        opacity: 0,
-        scale: 0.95,
-        filter: "blur(10px)",
-      }}
-      animate={{
-        opacity: 1,
-        scale: 1,
-        filter: "blur(0px)",
-      }}
-      transition={{ duration: 1 }}
-    >
-      <h1 className="text-4xl md:text-5xl md:font-semibold">ThangkaAI</h1>
-      <p className="px-5 w-full  mt-5 sm:w-2/3 lg:w-2/5 text-center md:mt-3 text-gray-500 text-md md:text-xl">
+    <div className="flex flex-col justify-center items-center h-[calc(100vh-4rem-2.5rem)] w-full">
+      <motion.div
+        className=""
+        initial={{
+          opacity: 0,
+          scale: 0.95,
+          filter: "blur(10px)",
+        }}
+        animate={{
+          opacity: 1,
+          scale: 1,
+          filter: "blur(0px)",
+        }}
+        transition={{ duration: 1 }}
+      >
+        <h1 className="text-4xl md:text-5xl md:font-semibold">ThangkaAI</h1>
+      </motion.div>
+      <motion.p
+        initial={{
+          opacity: 0,
+          scale: 0.95,
+          filter: "blur(10px)",
+        }}
+        animate={{
+          opacity: 1,
+          scale: 1,
+          filter: "blur(0px)",
+        }}
+        transition={{ duration: 1, delay: 0.35 }}
+        className="px-5 w-full  mt-5 sm:w-2/3 lg:w-2/5 text-center md:mt-3 text-gray-500 text-md md:text-xl"
+      >
         An AI platform transforming text prompts into stunning visual art with
         modern creativity. Unleash your imagination effortlessly!
-      </p>
-    </motion.div>
+      </motion.p>
+    </div>
   );
 }
