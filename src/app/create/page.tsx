c8020a32b60231275a6fe6c28890c904eeead450
Lobsang Norbu <50455820+lobnor2@users.noreply.github.com>
2024-11-30 09:31:24 -0500

    add create page

---
+++ src/app/create/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="border border-red-500 w-full h-dvh flex items-center justify-center">
+      hello
+    </div>
+  );
+};
+
+export default Page;
