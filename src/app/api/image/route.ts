e8f51b15fa1fce0523ad659ded69eea4d6dd34fa
Lobsang Norbu <50455820+lobnor2@users.noreply.github.com>
2025-01-30 08:12:15 -0500

    fix promise error

--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -1,16 +1,25 @@
 import { authOptions } from "@/utils/authOptions";
+import { prisma } from "@/utils/prisma";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const session = getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
   if (!session) {
     return NextResponse.json({ error: "Unauthorized", status: 401 });
   }
   const { prompt } = await request.json();
 
+  const user = await prisma.user.findUnique({
+    where: { id: session.user.id },
+  });
+
+  if (!user) {
+    return NextResponse.json({ error: "No user found", status: 401 });
+  }
+
   function generateRandomNumber(): number {
-    return Math.floor(Math.random() * 10000) + 1;
+    return Math.floor(Math.random() * 1000000) + 1;
   }
   const randomSeed = generateRandomNumber();
 
@@ -19,6 +28,14 @@ export async function POST(request: NextRequest) {
   )}?seed=${randomSeed}&width=512&height=512&nologo=True`;
 
   await fetch(imageURL);
+
+  await prisma.post.create({
+    data: {
+      prompt: prompt,
+      url: imageURL,
+      seed: randomSeed,
+    },
+  });
 
   return NextResponse.json({ url: imageURL });
 }
