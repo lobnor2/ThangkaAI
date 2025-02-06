7ce156265c8e34e3268789c9332b95c6f7b6a393
Lobsang Norbu <50455820+lobnor2@users.noreply.github.com>
2025-02-06 09:00:31 -0500

    add fetch function

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const fetchPosts = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch("api/image");
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+  return (
+    <div className="w-full min-h-dvh flex justify-center items-center p-3 pt-[72px]">
+      {loading ? "loading..." : "Profile"}
+    </div>
+  );
 };
 
 export default Profile;
