07714460550a035cf6ac9913ea3a71f823dd3cfc
Lobsang Norbu <50455820+lobnor2@users.noreply.github.com>
2025-02-08 23:23:51 -0500

    add styles for not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,15 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
 
 const NotFound = () => {
   return (
-    <div>
-      <h2>Not Found</h2>
+    <div className=" h-[calc(100vh-110px)] flex justify-center items-center flex-col">
+      <h2 className="text-5xl">Not Found</h2>
       <p>Could not find request resource</p>
-      <Link href={"/"}>Return Home</Link>
+      <Link href={"/"} className="mt-5">
+        <Button>Return Home</Button>
+      </Link>
     </div>
   );
 };
